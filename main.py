247e46b5bae4685c85802bb4e403a5286b713eb5
kyles <kylesv@hotmail.com>
2024-02-19 19:41:18 -0600

    day 22: add description, imgages, and links

--- main.py
+++ main.py
@@ -21,14 +21,20 @@ Below are some of the apps i have created!
 st.write(content2)
 
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,.5,1.5])
 
 df = pandas.read_csv('data.csv', sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f'[source code]({row["url"]})')
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f'[source code]({row["url"]})')
